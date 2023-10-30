8c5a4baac8d12e6daaf57853ad4dec89f7fcf8f2
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-10-30 02:48:50 -0700

    Emailing

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ function toggleModal() {
     isModalOpen = true
     document.body.classList += " modal--open"
 }
+
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+
+    emailjs
+        .sendForm(
+            'service_wp048se',
+            'template_si8f8fp',
+            event.target,
+            'IoiwiKiSBN_lWY9MM'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible")
+            setTimeout(success.classList += " modal__overlay--visible", 3000)
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                "The email service is temporatily unavailable. Please contact me directly on malidzulfiqar@gmail.com"
+            )
+        }) 
+}
