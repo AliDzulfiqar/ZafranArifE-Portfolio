098a59f7993204601a625f9baf4f21398eb05e3e
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-12-20 11:00:16 -0800

    Fixed theme problems

--- e-portfolio/src/utils/theme.js
+++ e-portfolio/src/utils/theme.js
@@ -1,11 +1,11 @@
 export const lightTheme = {
     white:"#FFFbf8",
     black:"#231f20",
-    primary: "004aad",
+    primary: "#004aad",
 }
 
 export const darkTheme = {
     white:"#231f20",
     black:"#fffbf8",
-    primary: "0277b5",
+    primary: "#0277b5",
 }
