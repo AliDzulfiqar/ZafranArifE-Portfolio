51efefb54a3c68b54af0af79f7dad2a9575f71dc
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-12-20 10:29:20 -0800

    Added navbar

--- e-portfolio/src/App.js
+++ e-portfolio/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from './utils/theme';
+import { useState } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <>
-    </>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <NavBar check={darkMode} change={() => setDarkMode(!darkMode)} />
+    </ThemeProvider>
   );
 }
 
---
+++ e-portfolio/src/components/NavBar/index.js
@@ -0,0 +1,216 @@
+import styled from "styled-components";
+import { Link as LinkR } from "react-scroll";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { Switch } from "@mui/material";
+
+export const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  @media (max-width: 960px) {
+    trastion: 0.8s all ease;
+  }
+`;
+
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1200px;
+`;
+
+export const NavLogo = styled(LinkR)`
+  width: 80%;
+  padding: 0 6px;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  text-decoration: none;
+  @media (max-width: 640px) {
+    padding: 0 0px;
+  }
+`;
+
+export const Span = styled.div`
+  padding: 0 4px;
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+export const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavLink = styled.a`
+  color: ${({ theme }) => theme.black};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  :hover {
+    color: ${({ theme }) => theme.text_primary};
+  }
+
+  &.active {
+    border-bottom: 2px solid ${({ theme }) => theme.button};
+  }
+`;
+
+export const MobileIcon = styled.div`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.5rem;
+    cursor: pointer;
+    color: ${({ theme }) => theme.black};
+  }
+`;
+
+export const MobileMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+  position: absolute;
+  top: 80px;
+  right: 0;
+  width: 100%;
+  padding: 12px 40px 24px 40px;
+  background: ${({ theme }) => theme.bg};
+  transition: all 0.6s ease-in-out;
+  transform: ${({ isOpen }) =>
+    isOpen ? "translateY(0)" : "translateY(-100%)"};
+  border-radius: 0 0 20px 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  z-index: ${({ isOpen }) => (isOpen ? "1000" : "-1000")};
+`;
+
+export const MobileMenuItems = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  list-style: none;
+  width: 100%;
+  height: 100%;
+`;
+
+export const MobileMenuLink = styled(LinkR)`
+  color: ${({ theme }) => theme.black};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  :hover {
+    color: ${({ theme }) => theme.text_primary};
+  }
+
+  &.active {
+    border-bottom: 2px solid ${({ theme }) => theme.black};
+  }
+`;
+
+export const MobileLink = styled.a`
+  color: ${({ theme }) => theme.black};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  :hover {
+    color: ${({ theme }) => theme.text_primary};
+  }
+
+  &.active {
+    border-bottom: 2px solid ${({ theme }) => theme.black};
+  }
+`;
+
+const NavBar = ({ check, change }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Nav>
+      <NavbarContainer>
+        <MobileIcon>
+          <FaBars
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          <Switch defaultChecked onChange={change} checked={check} />
+        </NavItems>
+        {isOpen && (
+          <MobileMenu isOpen={isOpen}>
+            <MobileLink
+              href="#about"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              About
+            </MobileLink>
+            <MobileLink
+              href="#skills"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              Skills
+            </MobileLink>
+            <MobileLink
+              href="#experience"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              Experience
+            </MobileLink>
+            <MobileLink
+              href="#projects"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              Projects
+            </MobileLink>
+            <Switch defaultChecked onChange={change} checked={check} />
+          </MobileMenu>
+        )}
+      </NavbarContainer>
+    </Nav>
+  );
+};
+
+export default NavBar;
