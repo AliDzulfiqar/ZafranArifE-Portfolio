d1838633e21f5a26c35c3e21cbd784d21c3525cb
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-12-20 10:00:50 -0800

    Updated data.js

---
+++ e-portfolio/src/data/data.js
@@ -0,0 +1,118 @@
+export const Bio = {
+  name: "Zafran Akhmadery Arif",
+  headline: "Leadership, Mathematics, and Digital Literacy.",
+  description: [
+    "Zafran, a young mathematician from Washington State University, is a prominent leader. Notably serving as the Secretary-General of the Overseas Indonesian Students' Association Alliance (OISAA/PPI Dunia).",
+    "He is a dedicated digital literacy enthusiast with a strong passion in data science and machine learning, driven to apply his knowledge to real-world problems.",
+    "With his unique combination of leadership, mathematical prowess, and passion for digital literacy, Zafran is poised to make significant contributions in both academic and professional domains.",
+  ],
+  github: "https://github.com/zafranarif99",
+  resume:
+    "https://drive.google.com/file/d/1LBKtZ0Wz-NKke63ddQlWd6b6MvNS30JD/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/zafranarif99/",
+  instagram: "https://www.instagram.com/zafranarif99",
+};
+
+export const skills = [
+  {
+    name: "R",
+    image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+  },
+  {
+    name: "MATLAB",
+    image: "https://i.im.ge/2023/11/23/AV01t4.icons8-css-logo-480.png",
+  },
+  {
+    name: "Python",
+    image:
+      "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+  },
+  {
+    name: "Excel",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+  },
+  {
+    name: "Latex",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+  },
+];
+
+export const activities = [
+  {
+    title: "Secretary-General of OISAA",
+    description: "Led the organization to new heights from 2022 to 2023.",
+    gallery: [
+      {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/C_Logo.png/640px-C_Logo.png",
+      },
+      {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/640px-ISO_C%2B%2B_Logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "The 42nd ASEAN Summit",
+    description: "Led the organization to new heights from 2022 to 2023.",
+    gallery: [
+      {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/C_Logo.png/640px-C_Logo.png",
+      },
+      {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/640px-ISO_C%2B%2B_Logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Dairy Microbiology Laboratory - School of Food Science WSU",
+    description: "Led the organization to new heights from 2022 to 2023.",
+    gallery: [
+      {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/C_Logo.png/640px-C_Logo.png",
+      },
+      {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/640px-ISO_C%2B%2B_Logo.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Invited as a speaker in various seminars/conferences",
+    description: "Led the organization to new heights from 2022 to 2023.",
+    gallery: [
+      {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/C_Logo.png/640px-C_Logo.png",
+      },
+      {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/640px-ISO_C%2B%2B_Logo.svg.png",
+      },
+    ],
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Linear Optimization for Multi-period Team Assignment Planning",
+    description:
+      "As a consultant, I needed to maximize the total productivity of a company by assigning 50 available staff members to the projects subject to several conditions. There were five different projects for nine weeks and I had the data of the score/rating of each staff member for their productivity, creativity, and teamwork.",
+    tags: ["MATLAB", "LaTeX"],
+    link: "https://drive.google.com/file/d/1gvJRqC7LC3-DhltSZvI45SDaLk6DOvHr/view?usp=sharing",
+  },
+  {
+    id: 10,
+    title: "Linear Algebra: The Foundation of Image Processing and Artificial Intelligence",
+    description:
+      "A book that I’m currently writing to give a brief explanation of linear algebra and its application in real life.",
+    tags: ["LaTeX"],
+    link: "https://drive.google.com/file/d/1P7uGkXIOjSE3-bJXOAdiRl1PJ2ePAFJH/view?usp=sharing",
+  },
+];
